dde9f2841cac75f1920c84bba1801dfd6f16e171
Teoslip <tjachiet@gmail.com>
2025-06-29 18:02:21 +0200

    Make displayHome function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './style.css';
 console.log('here');
+
+const content = document.querySelector('.content');
+
+function displayHome() {
+    const homeTitle = document.createElement('h2');
+    const homeParagraph = document.createElement('p');
+
+    homeParagraph.classList.add('home');
+    homeTitle.classList.add('home');
+    homeTitle.innerHTML = 'Le <strong>meilleur</strong> restaurant <strong>quantique</strong> du secteur <strong>tellurique</strong>';
+    homeParagraph.innerHTML = "Alignez vos chakras dans cet espace anti 5g et ouvert aux tachyons positifs. <br>Jamais manger n'a été aussi cosmique";
+
+    content.appendChild(homeTitle);
+    content.appendChild(homeParagraph);
+}
+
+displayHome();
